26dc448ea314754d318adce20b1930919eb4ae86
Maulik <139668060+maulik-4200@users.noreply.github.com>
2024-10-15 16:30:31 +0530

    Outputs Added

--- P13Controller.java
+++ P13Controller.java
@@ -2,12 +2,9 @@ package com.example.p13;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class P13Controller {
